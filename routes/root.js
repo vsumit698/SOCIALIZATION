01c9fbdc90afb069f76c9d8ac11362a99586f174
vsumit698 <vsumit698@gmail.com>
2020-02-28 03:13:24 +0530

    third

--- routes/root.js
+++ routes/root.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const userRoute = require('./user');
 const controller = require('../controllers/action');
 router.get('/',controller.home);
-router.get('/user/',userRoute);
+router.get('/user',userRoute);
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const controller  = require('../controllers/action');
+
 router.get('/post',controller.post);
-router.get('/like',controller.like);
+// router.get('/like',controller.like);
 
 
 module.exports = router;
